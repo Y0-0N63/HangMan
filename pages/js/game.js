9e69442096374d70f22a269873b777ad091118dc
Y0-0N63 <boyun1625@naver.com>
2024-07-28 23:44:37 +0900

    Pause 팝업창 추가

--- pages/js/game.js
+++ pages/js/game.js
@@ -5,3 +5,20 @@ document.querySelector('#others__how').addEventListener('click', function () {
 document.querySelector('.content_close').addEventListener('click', function () {
   document.querySelector('.play_popup').style.display = 'none';
 });
+
+document.querySelector('#others__pause').addEventListener('click', function () {
+  document.querySelector('.pause_popup').style.display = 'flex';
+});
+
+document.querySelector('.content_close').addEventListener('click', function () {
+  document.querySelector('.pause_popup').style.display = 'none';
+});
+
+document.querySelector('#pause__resume').addEventListener('click', function () {
+  document.querySelector('.pause_popup').style.display = 'none';
+  // 게임을 다시 시작하는 로직을 추가할 수 있습니다
+});
+
+document.querySelector('#pause__quit').addEventListener('click', function () {
+  // 게임을 종료하는 로직을 추가할 수 있습니다
+});
