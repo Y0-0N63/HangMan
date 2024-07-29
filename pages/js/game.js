5ed14464db2f3a70bb9611f4e730a1a878d1a077
Y0-0N63 <boyun1625@naver.com>
2024-07-30 00:11:12 +0900

    게임 종료 조건 추가하여 코드 수정

--- pages/js/game.js
+++ pages/js/game.js
@@ -17,12 +17,13 @@ const words = [
   'HORIZON',
   'BALANCE',
   'DYNAMIC',
-  'SPACIAL',
+  'SPECIAL',
   'TRAVEL',
   'SERENITY',
   'ADVENTURE',
 ];
 
+// 단어 출제
 function selectVoca() {
   const vocaIndex = Math.floor(Math.random() * words.length);
   return words[vocaIndex];
@@ -30,6 +31,7 @@ function selectVoca() {
 
 const selectedVoca = selectVoca();
 
+// 페이지가 로드되면 단어의 길이만큼 '_' 표시
 function makeBlank() {
   const blanks = document.querySelector('.right__blank');
   blanks.innerHTML = '';
@@ -41,11 +43,67 @@ function makeBlank() {
   }
 }
 
-// 페이지가 로드되면 단어의 길이만큼 '_' 표시
 document.addEventListener('DOMContentLoaded', function () {
   makeBlank();
 });
 
+// 게임 종료
+let wrong = 0;
+const maxWrong = 6;
+
+function gameOver() {
+  const findAll = Array.from(
+    document.querySelectorAll('.right__blank span')
+  ).every((span) => span.textContent !== '_');
+
+  if (findAll) {
+    showGameOver('You Win!');
+  } else if (wrong > maxWrong) {
+    showGameOver('Game Over');
+  }
+}
+
+function showGameOver(message) {
+  const popup = document.querySelector('.popup__gameOver');
+  const messageElem = document.querySelector('.gameOver__message');
+  messageElem.textContent = message;
+  document.querySelector('.play__popup').style.display = 'flex';
+  popup.style.display = 'flex';
+}
+
+// 알파벳 버튼 클릭
+function clickAlphabet(event) {
+  const button = event.target;
+  const letter = button.textContent;
+
+  if (!button.classList.contains('buttons__alphabet')) return;
+  button.disabled = true;
+
+  if (selectedVoca.includes(letter)) {
+    Array.from(document.querySelectorAll('.right__blank span')).forEach(
+      (span, index) => {
+        if (selectedVoca[index] === letter) {
+          span.textContent = letter;
+        }
+      }
+    );
+
+    button.style.backgroundColor = '#0a0a0a';
+    button.style.color = '#50adff';
+  } else {
+    button.style.backgroundColor = '#0a0a0a';
+    button.style.color = '#ff696f';
+    wrong++;
+  }
+
+  gameOver();
+}
+
+document.querySelectorAll('.keyboard__buttons button').forEach((button) => {
+  button.addEventListener('click', clickAlphabet);
+});
+
+// 팝업창들
 document.querySelector('#others__how').addEventListener('click', function () {
   document.querySelector('.play__popup').style.display = 'flex';
   document.querySelector('.popup__howToPlay').style.display = 'flex';
@@ -73,6 +131,9 @@ document.querySelector('#pause__resume').addEventListener('click', function () {
   document.querySelector('.play__popup').style.display = 'none';
 });
 
-document.querySelector('#pause__quit').addEventListener('click', function () {
-  // TODO: Add logic for quitting the game
-});
+document
+  .querySelector('.gameOver__close')
+  .addEventListener('click', function () {
+    document.querySelector('.popup__gameOver').style.display = 'none';
+    document.querySelector('.play__popup').style.display = 'none';
+  });
